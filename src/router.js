5a714c4dfce6d2c984943252ec0aea78e937dc92
Ry <OfficiallyRy@outlook.com>
2023-04-27 14:46:20 -0600

    Beginning of Commission

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import { authGuard } from '@bcwdev/auth0provider-client'
+
+function loadPage(page) {
+  return () => import(`./pages/${page}.vue`)
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: loadPage('HomePage')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: loadPage('AboutPage')
+  },
+]
+
+export const router = createRouter({
+  linkActiveClass: 'router-link-active',
+  linkExactActiveClass: 'router-link-exact-active',
+  history: createWebHashHistory(),
+  routes
+})
