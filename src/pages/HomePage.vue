6bcf8cbb5344ef9121b55bcffebda51922d28e26
Ry <OfficiallyRy@outlook.com>
2023-04-29 20:48:02 -0600

    Added all necessary fonts for project, and made custom classes for each

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md-5">
 
+      </div>
+      <div class="col-12 col-md-7">
+
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-md-6">
+        <span class="fs-2 text-purple">
+
+        </span>
+      </div>
+      <div class="col-12 col-md-6">
+        <span class="fs-2 text-purple">
+
+        </span>
+      </div>
+      <div class="col-12 d-md-none">
+        <span class="fs-2 text-purple">&</span>
+      </div>
+      <div class="col-12">
+        <span class="fs-2 text-purple">
+
+        </span>
+      </div>
+    </div>
+    <div class="row">
+
+    </div>
   </div>
 </template>
 
