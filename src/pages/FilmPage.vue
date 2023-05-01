8fda52f60983855930b2ca9ea60cdc4a7a304cbf
Ry <OfficiallyRy@outlook.com>
2023-05-01 13:38:52 -0600

    Film Page is all perfectly stubbed out with placeholder info

--- src/pages/FilmPage.vue
+++ src/pages/FilmPage.vue
@@ -1,88 +1,88 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mt-3 mb-4 text-center">
         <span class="fs-1 jost spacing text-purple">
           HEAD OF ART DEPARTMENT
         </span>
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mt-5 mb-4 text-center">
         <span class="fs-1 jost spacing text-purple">
           FIRST ASSISTANT DIRECTOR
         </span>
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mt-5 mb-4 text-center">
         <span class="fs-1 jost spacing text-purple">
           SPECIAL EFFECTS MAKEUP
         </span>
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
-      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content center">
-        <span class="fs-2">
+      <div class="col-12 col-md-6 d-flex flex-column align-items-center justify-content-center pb-3">
+        <span class="fs-2 jost pb-1">
           <i>
-
+            Movie Title
           </i>
         </span>
-        <img src="" alt="" class="w-100 rounded border border-dark elevation-2">
+        <img src="https://placehold.co/900x400" alt="" class="w-100 rounded border border-dark elevation-2">
       </div>
     </div>
   </div>
